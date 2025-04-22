f40ef58a26fc4d08da85adea39648d38013bf77a
Saiguer10 <eljosemsp@gmail.com>
2025-04-22 16:08:06 +0200

    commit creacion clase

--- src/main/java/com/homework/library/Author.java
+++ src/main/java/com/homework/library/Author.java
@@ -1,4 +1,45 @@
 package com.homework.library;
 
 public class Author {
+    // Variables privadas
+    private int authorId; // Autoincrementada
+    private String name;
+    private String email;
+    private Book authorBook; // Relación uno a uno con la clase Libro
+    private static int nextId = 1; // Para autoincrementar el ID
+
+    public Author(String name, String email, Book authorBook) {
+        this.authorId = nextId++;
+        this.name = name;
+        this.email = email;
+        this.authorBook = authorBook;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Book getAuthorBook() {
+        return authorBook;
+    }
+
+    public void setAuthorBook(Book authorBook) {
+        this.authorBook = authorBook;
+    }
 }
