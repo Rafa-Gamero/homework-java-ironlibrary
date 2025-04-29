76750f97edb484e88be69b83f3369facb10786eb
Saiguer10 <eljosemsp@gmail.com>
2025-04-29 10:33:51 +0200

    Commit funcionalidad ok

---
+++ src/main/java/com/homework/library/repositories/IssueRepository.java
@@ -0,0 +1,10 @@
+package com.homework.library.repositories;
+
+
+import com.homework.library.models.Issue;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface IssueRepository extends JpaRepository<Issue, Long> {
+    List<Issue> findByStudent_Usn(String usn);
+}
